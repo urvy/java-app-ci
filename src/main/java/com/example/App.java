7228b219591da753f64e1114ff321a34bcddee3c
urvy <urvy2002@gmail.com>
2025-11-12 11:38:06 +0000

    webhook

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Urvy second time successfull, Jenkins CI/CD!");
+        System.out.println("Hello Urvy second time successfull whattt is this, Jenkins CI/CD!");
     }
 }
