36c6258aded76ad821dc78334f131a37731325dd
urvy <81502671+urvy@users.noreply.github.com>
2025-11-13 10:00:36 +0530

    comment add

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Wassp, good morning, Jenkins CI/CD!, webhook works fine!");
+        System.out.println("Hello Wassp, good morning, Jenkins CI/CD!, webhook works fine!"); #hi this ne
     }
 }
